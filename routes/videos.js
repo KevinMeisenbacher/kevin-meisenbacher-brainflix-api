b5ad6e7774695c7c95e2a3a3a2a37047545c97a0
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-04-22 21:16:11 -0400

    Setup server and completely migrated to it

--- routes/videos.js
+++ routes/videos.js
@@ -1,19 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const {v4: uuid} = require('uuid');
+
+const videoDetails = require('../data/videos.json');
+
+const videos = (
+    videoDetails.map(video => (
+    {
+        "id": video.id,
+        "title": video.title,
+        "channel": video.channel,
+        "image": video.image
+    }
+    ))
+)
 
 router.get('/', (req, res) => {
-    res.send('get all videos');
+    res.send(videos);
 });
 
-const videos = require('../data/videos.json');
-
-router.get('/videos', (req, res) => {
-    res.json(videos);
-});
-
-router.get('/videos/:video', (req, res) => {
-    const video = req.params.video;
+router.get('/:videoId', (req, res) => {
+    const video = videoDetails.find(video => video.id === req.params.videoId);
     res.send(video);
+});
+
+router.post('/:videoId/comments', (req, res, next) => {
+    const video = videoDetails.find(video => video.id === req.params.videoId);
+    const newComment = {
+        id: uuid(),
+        name: 'Kevin',
+        comment: req.body.comment,
+        likes: 0,
+        timestamp: new Date().getTime()
+    };
+    video.comments.push(newComment);
+    res.send(video.comments);
+    next();
+}); 
+
+router.get('/:videoId/comments/:commentId', (req, res) => {
+    const video = videoDetails.find(video => video.id === req.params.videoId);
+    const comment = video.comments.find(comment => comment.id === req.params.commentId);
+    console.log(comment);
+    res.send(comment);
+})
+
+router.delete('/:videoId/comments/:commentId', (req, res) => {
+    const video = videoDetails.find(video => video.id === req.params.videoId);
+    const comment = video.comments.find(comment => comment.id === req.params.commentId);
+    video.comments = video.comments.filter(
+        ({id: commentId}) => commentId !== comment.id
+        // crapComment => comment !== crapComment
+    );
+    // fs.writeFileSync('filteredVideos.json', res.locals);
+    console.log(`${comment} deleted`);
+    res.send(`${comment} deleted`);
+    // res.json(res.locals.video.comments);
+    // res.send({status: `${id} deleted`});
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(express.static('public'));
 const videoRoutes = require('./routes/videos');
 
+app.use(cors());
+app.use(express.json());
 app.use('/videos', videoRoutes);
 
 app.get('/', (req, res) => {
